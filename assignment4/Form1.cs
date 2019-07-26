1d7df5df472aeae4c8e914f26a5149fa75b1cc3e
300728876 <300728876@PR-E216-55>
2019-07-26 19:21:55 -0400

    create calculator

--- assignment4/Form1.cs
+++ assignment4/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace assignment4
 {
-    public partial class Form1 : Form
+    public partial class BmiCalculator : Form
     {
-        public Form1()
+        public BmiCalculator()
         {
             InitializeComponent();
         }
